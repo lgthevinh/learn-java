f86db4367a6123739872b73c1a2292d098e9ff08
lgthvinh <everwellmax@gmail.com>
2025-06-16 15:48:56 +0700

    SqliteDatabase

--- src/main/java/com/edge/example/database/SqliteDatabase.java
+++ src/main/java/com/edge/example/database/SqliteDatabase.java
@@ -1,19 +1,20 @@
 package com.edge.example.database;
 
-import com.edge.example.core.DaoField;
+import com.edge.example.core.DaoName;
+import com.edge.example.model.SqlModelAdapter;
 import com.edge.example.model.BaseModel;
-
-import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.util.List;
 
-public class SqliteDatabase<T extends BaseModel> extends AbstractDatabase<T, ResultSet> {
+public class SqliteDatabase<T extends BaseModel> extends AbstractDatabase<T, SqlModelAdapter<T>> {
 
     private Connection connection;
+    private final Class<T> modelClass;
 
-    public SqliteDatabase() {
+    public SqliteDatabase(Class<T> modelClass) {
+        this.modelClass = modelClass;
         try {
             // Initialize SQLite connection
             connection = DriverManager.getConnection("jdbc:sqlite:database.db");
@@ -23,65 +24,103 @@ public class SqliteDatabase<T extends BaseModel> extends AbstractDatabase<T, Res
     }
 
     @Override
-    protected void insertImpl(ResultSet dbModel) {
-
-    }
-
-    @Override
-    protected ResultSet readImpl(String id) {
-        return null;
-    }
-
-    @Override
-    protected void updateImpl(ResultSet dbModel) {
-
-    }
-
-    @Override
-    protected void deleteImpl(ResultSet dbModel) {
-
-    }
-
-    @Override
-    protected List<ResultSet> queryImpl(String query) {
-        return null;
-    }
-
-    @Override
-    protected T convertToBaseModel(ResultSet dbModel) {
-        if (dbModel == null) {
-            return null;
+    protected void insertImpl(SqlModelAdapter<T> dbModel) {
+        String query = "INSERT INTO " + modelClass.getAnnotation(DaoName.class).name() + " (";
+        StringBuilder columns = new StringBuilder();
+        StringBuilder placeholders = new StringBuilder();
+        for (String key : dbModel.getFields().keySet()) {
+            columns.append(key).append(", ");
+            placeholders.append("?, ");
         }
+        query += columns.substring(0, columns.length() - 2) + ") VALUES (" + placeholders.substring(0, placeholders.length() - 2) + ")";
 
-        try {
-            T model = modelClass.getDeclaredConstructor().newInstance();
-            Field[] fields = modelClass.getDeclaredFields();
-
-            for (Field field : fields) {
-                field.setAccessible(true);
-                String fieldName = field.getName();
-
-                if (field.isAnnotationPresent(DaoField.class)) {
-                    DaoField daoField = field.getAnnotation(DaoField.class);
-                    fieldName = daoField.name(); // Use the annotated name
-                }
-
-                // Assuming dbModel has a method to get value by column name
-                Object value = dbModel.getObject(fieldName);
-                if (value != null) {
-                    field.set(model, value);
-                }
+        try (var preparedStatement = connection.prepareStatement(query)) {
+            int index = 1;
+            for (Object value : dbModel.getFields().values()) {
+                preparedStatement.setObject(index++, value);
             }
-
+            preparedStatement.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    @Override
+    protected SqlModelAdapter<T> readImpl(String uid) {
+        String query = "SELECT * FROM " + modelClass.getAnnotation(DaoName.class).name() + " WHERE uid = ?";
+        try (var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, uid);
+            ResultSet result = preparedStatement.executeQuery();
+
+            SqlModelAdapter<T> dbModel = new SqlModelAdapter<>(modelClass);
+            dbModel.fromResultSet(result);
+            return dbModel;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
         return null;
     }
 
     @Override
-    protected ResultSet convertToDbModel(T model) {
-        return null; // SQLite does not use ResultSet for inserts, updates, or deletes
+    protected void updateImpl(SqlModelAdapter<T> dbModel) {
+        String query = "UPDATE " + modelClass.getAnnotation(DaoName.class).name() + " SET ";
+        StringBuilder setClause = new StringBuilder();
+        for (String key : dbModel.getFields().keySet()) {
+            setClause.append(key).append(" = ?, ");
+        }
+        query += setClause.substring(0, setClause.length() - 2) + " WHERE uid = ?";
+
+        try (var preparedStatement = connection.prepareStatement(query)) {
+            int index = 1;
+            for (Object value : dbModel.getFields().values()) {
+                preparedStatement.setObject(index++, value);
+            }
+            preparedStatement.setString(index, dbModel.getFields().get("uid").toString());
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void deleteImpl(SqlModelAdapter<T> dbModel) {
+        String query = "DELETE FROM " + modelClass.getAnnotation(DaoName.class).name() + " WHERE uid = ?";
+        try (var preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, dbModel.getFields().get("uid").toString());
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected List<SqlModelAdapter<T>> queryImpl(String query) {
+        try (var statement = connection.createStatement();
+             var resultSet = statement.executeQuery(query)) {
+            List<SqlModelAdapter<T>> results = new java.util.ArrayList<>();
+            while (resultSet.next()) {
+                SqlModelAdapter<T> dbModel = new SqlModelAdapter<>(modelClass);
+                dbModel.fromResultSet(resultSet);
+                results.add(dbModel);
+            }
+            return results;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return java.util.Collections.emptyList();
+        }
+    }
+
+    @Override
+    protected T convertToBaseModel(SqlModelAdapter<T> dbModel) {
+        return dbModel.toTModel();
+    }
+
+    @Override
+    protected SqlModelAdapter<T> convertToDbModel(T model) {
+        SqlModelAdapter<T> dbModel = new SqlModelAdapter<>(modelClass);
+        dbModel.fromTModel(model);
+        return dbModel;
     }
 }
